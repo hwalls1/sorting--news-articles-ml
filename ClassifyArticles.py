53a8299af92575e44e657c7f04df0107716711dc
Harrison Walls <hjw1145@gmail.com>
2023-08-24 10:59:21 -0400

    modernized old project

--- ClassifyArticles.py
+++ ClassifyArticles.py
@@ -1,64 +1,53 @@
-# Harrison Walls Junior IS
-# Requires Python >= 2.7
-# NumPy >= 1.8.2
-# SciPy >= 0.13.3
-# Refernece: http://scikit-learn.org/stable/index.htm
-
-
-from __future__ import print_function
-
 import numpy as np
-import matplotlib.pyplot as plt
-
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn import metrics
 
+
+def fetch_dataset(categories, subset='train', remove=('header', 'footers', 'quotes')):
+    """Fetch 20 newsgroups dataset."""
+    return fetch_20newsgroups(subset=subset, categories=categories, shuffle=True,
+                              random_state=42, remove=remove)
+
+
+def benchmark(clf, X_train, y_train, X_test, y_test):
+    """Train and test a classifier; return metrics."""
+    clf.fit(X_train, y_train)
+    pred = clf.predict(X_test)
+    score = metrics.accuracy_score(y_test, pred)
+    
+    # You can add more metrics here as needed
+    return {'name': clf.__class__.__name__, 'score': score}
+
+
 def main():
-
-# Uploads categories from the "20 Newsgroups" dataset.
-    categories = ['rec.motorcycles','rec.sport.baseball','rec.sport.hockey',
+    # Define categories
+    categories = [
+        'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey',
         'sci.space', 'sci.crypt', 'sci.electronics', 'sci.med',
-        'talk.politics.misc','talk.politics.guns','talk.politics.mideast'
-        ]
+        'talk.politics.misc', 'talk.politics.guns', 'talk.politics.mideast'
+    ]
 
-    remove = ('header','footers','quotes')
+    print("Loading 20 newsgroups dataset for categories:", categories)
+    
+    # Fetch data
+    data_train = fetch_dataset(categories, subset='train')
+    data_test = fetch_dataset(categories, subset='test')
 
-    print("Loading 20 newsgroups dataset for categories:")
-    print(categories)
-
-# Loads training dataset from "20 newsgroups"
-    data_train = fetch_20newsgroups(subset='train', categories=categories,
-                                shuffle=True, random_state=42,
-                                remove=remove)
-
-# Loads test dataset from "20 newsgroups"
-    data_test = fetch_20newsgroups(subset='test', categories=categories,
-                               shuffle=True, random_state=42,
-                               remove=remove)
-
-    print("%d categories" % len(categories))
-
-    print("Extracting features from the training data using a sparse vectorizer")
-
-    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                                 stop_words='english')
+    # Vectorize data
+    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
     X_train = vectorizer.fit_transform(data_train.data)
-    print("Number of samples: %d, Number of features: %d" % X_train.shape)
-
-    print("Extracting features from the test data using the same vectorizer")
     X_test = vectorizer.transform(data_test.data)
-    print("Number of samples: %d, Number of features: %d" % X_test.shape)
-    print()
 
-    results = []
-
-
-# Train Naive Bayes classifiers
-    print("Naive Bayes")
-    results.append(benchmark(MultinomialNB(alpha=.01)))
-    results.append(benchmark(BernoulliNB(alpha=.01)))
+    print("Training samples:", X_train.shape[0], ", Features:", X_train.shape[1])
+    print("Testing samples:", X_test.shape[0], ", Features:", X_test.shape[1])
+    
+    # Train and benchmark classifiers
+    classifiers = [MultinomialNB(alpha=.01), BernoulliNB(alpha=.01)]
+    for clf in classifiers:
+        result = benchmark(clf, X_train, data_train.target, X_test, data_test.target)
+        print(f"{result['name']} Accuracy: {result['score']:.4f}")
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,6 +1,40 @@
-# Junior-Independent-Study
-Built an application, using Python and Scikit-Learn library, to sort new articles into three categories (technology, politics, sports).
+# 20 Newsgroups Classifier
 
-# Requires Python >= 2.7
-# NumPy >= 1.8.2
-# SciPy >= 0.13.3
+This program classifies articles from the "20 Newsgroups" dataset into predefined categories using Naive Bayes classifiers.
+
+## Dependencies
+
+- **Python 3**
+- **NumPy** (>= 1.8.2)
+- **SciPy** (>= 0.13.3)
+- **scikit-learn**
+
+For visualization (if implemented in future iterations):
+- **matplotlib**
+
+Reference for `scikit-learn`: [Official Documentation](http://scikit-learn.org/stable/index.html)
+
+## Overview
+
+The script fetches articles from the "20 Newsgroups" dataset and focuses on classifying them into the following categories:
+- `rec.motorcycles`
+- `rec.sport.baseball`
+- `rec.sport.hockey`
+- `sci.space`
+- `sci.crypt`
+- `sci.electronics`
+- `sci.med`
+- `talk.politics.misc`
+- `talk.politics.guns`
+- `talk.politics.mideast`
+
+The classifiers utilized for this task are:
+- Multinomial Naive Bayes
+- Bernoulli Naive Bayes
+
+## Usage
+
+To run the script, navigate to the directory containing the script and execute:
+
+```bash
+python ClassifyArticles.py
